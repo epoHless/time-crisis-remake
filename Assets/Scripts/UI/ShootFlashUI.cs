20c23a12d3fe5e27033afc22731d2ef7998318a5
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2023-06-25 15:19:15 +0200

    Added flashing screen

---
+++ Assets/Scripts/UI/ShootFlashUI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup)), DisallowMultipleComponent]
+public class ShootFlashUI : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    private void OnEnable()
+    {
+        EventManager.OnBulletFired.AddListener(OnBulletFired);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnBulletFired.RemoveListener(OnBulletFired);
+    }
+
+    private void OnBulletFired(int _value)
+    {
+        canvasGroup.DOFade(1, 0.05f).SetLoops(2, LoopType.Yoyo);
+    }
+}
