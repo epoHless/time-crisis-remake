fe2fc030ae2f0207abe67d6d0babb0144bc2cfa7
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2023-06-26 10:15:10 +0200

    Added different prefab variants

--- Assets/Scripts/UI/Time/TimeGiverUI.cs
+++ Assets/Scripts/UI/Time/TimeGiverUI.cs
@@ -1,4 +1,6 @@
-﻿using DG.Tweening;
+﻿using System;
+using DG.Tweening;
+using Entities;
 using TMPro;
 using UnityEngine;
 
@@ -12,12 +14,17 @@ public class TimeGiverUI : MonoBehaviour
         EventManager.OnTimeGiverRequested.AddListener(OnTimeGiverRequested);
     }
 
+    private void OnDisable()
+    {
+        EventManager.OnTimeGiverRequested.RemoveListener(OnTimeGiverRequested);
+    }
+
     private void OnTimeGiverRequested(float _time, Vector3 _position)
     {
         time.text = $"+{_time} sec";
         transform.position = _position + offset;
         
-        time.DOFade(1, 1f).SetLoops(2, LoopType.Yoyo);
+        time.DOFade(1, .5f).SetLoops(2, LoopType.Yoyo);
         transform.DOMoveY(transform.position.y + 1f, 1f);
     }
 }
