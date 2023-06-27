e8eca3dc50bedeb89ab9c4ada3260c63b1c94c4e
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2023-06-27 15:07:54 +0200

    Added health UI & Camera Shake

---
+++ Assets/Scripts/Player/Camera/CameraShake.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        EventManager.OnDamageTaken.AddListener(OnDamageTaken);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnDamageTaken.RemoveListener(OnDamageTaken);
+    }
+
+    private void OnDamageTaken(int obj)
+    {
+        cam.transform.DOShakePosition(.15f, .4f, 30);
+    }
+}
---
+++ Assets/Scripts/UI/Health/HealthUI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    [SerializeField] private Image hitImage;
+    [SerializeField] private List<RectTransform> rectTransforms;
+
+    private void OnEnable()
+    {
+        EventManager.OnDamageTaken.AddListener(OnDamageTaken);
+        EventManager.OnGameStart.AddListener(Fill);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnDamageTaken.RemoveListener(OnDamageTaken);
+        EventManager.OnGameStart.RemoveListener(Fill);
+    }
+    
+    private void Fill()
+    {
+        foreach (var rectTransform in rectTransforms)
+        {
+            rectTransform.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnDamageTaken(int _currentHealth)
+    {
+        rectTransforms[_currentHealth].gameObject.SetActive(false);
+        hitImage.DOFade(.2f, 0.15f).SetLoops(2, LoopType.Yoyo);
+    }
+}
