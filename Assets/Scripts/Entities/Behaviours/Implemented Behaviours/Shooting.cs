4529ecd6405f69460f55514f343ffc3a6e25060d
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2023-06-28 22:01:42 +0200

    Added props in scene

--- Assets/Scripts/Entities/Behaviours/Implemented Behaviours/Shooting.cs
+++ Assets/Scripts/Entities/Behaviours/Implemented Behaviours/Shooting.cs
@@ -4,18 +4,19 @@ namespace Entities
 {
     public class Shooting : Behaviour
     {
-        [SerializeField] private float fireRate;
         [SerializeField] private Transform muzzle;
+        
+        private readonly float minFireRate = 0.5f;
+        private readonly float maxFireRate = 2f;
 
         private float currentTimer;
-        public override bool IsEnabled { get; set; }
 
         public override void Tick(Entity _entity)
         {
             if (currentTimer <= 0)
             {
                 EventManager.OnBulletRequested?.Invoke(muzzle);
-                currentTimer = fireRate;
+                currentTimer = Random.Range(minFireRate, maxFireRate);
             }
 
             currentTimer -= Time.deltaTime;
